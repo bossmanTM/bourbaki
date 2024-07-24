2979d3d575aed7a649b9d6dea984690f0ac9db0a
bossmanTM <chase2jackman@gmail.com>
2024-07-24 17:38:15 -0600

    test

--- src/number/cartesian/mod.rs
+++ src/number/cartesian/mod.rs
@@ -11,45 +11,45 @@ impl<T> Cartesian<T> {
 	}
 }
 
-impl<T: Signed> Signed for Cartesian<T> {
-	fn abs(&self) -> Cartesian<T> 
-	{
-		let mut abs_num = Cartesian::new();
-		for i in &self.coords {
-			abs_num.coords.push(i.abs())
-		}
-		abs_num
-	}
-
-	fn abs_sub(&self, other: &Self) -> Self {
-		let mut abs_num = Cartesian::new();
-		for i in 0..self.coords.len() {
-			abs_num.coords.push(self.coords[i].abs_sub(&other.coords[i]));
-		}
-		abs_num
-	}
-
-	fn signum(&self) -> Self {
-		let mut signum_num = Cartesian::new();
-		for i in &self.coords {
-			signum_num.coords.push(i.signum());
-		}
-		signum_num
-	}
-
-	fn is_negative(&self) -> bool {
-		let mut negative = false;
-		for i in &self.coords {
-			negative = i.is_negative() || negative
-		}
-		negative
-	}
-
-	fn is_positive(&self) -> bool {
-		let mut negative = false;
-		for i in &self.coords {
-			negative = i.is_negative() || negative
-		}
-		negative
-	}
-}
+//impl<T: Signed> Signed for Cartesian<T> {
+//	fn abs(&self) -> Cartesian<T> 
+//	{
+//		let mut abs_num = Cartesian::new();
+//		for i in &self.coords {
+//			abs_num.coords.push(i.abs())
+//		}
+//		abs_num
+//	}
+//
+//	fn abs_sub(&self, other: &Self) -> Self {
+//		let mut abs_num = Cartesian::new();
+//		for i in 0..self.coords.len() {
+//			abs_num.coords.push(self.coords[i].abs_sub(&other.coords[i]));
+//		}
+//		abs_num
+//	}
+//
+//	fn signum(&self) -> Self {
+//		let mut signum_num = Cartesian::new();
+//		for i in &self.coords {
+//			signum_num.coords.push(i.signum());
+//		}
+//		signum_num
+//	}
+//
+//	fn is_negative(&self) -> bool {
+//		let mut negative = false;
+//		for i in &self.coords {
+//			negative = i.is_negative() || negative
+//		}
+//		negative
+//	}
+//
+//	fn is_positive(&self) -> bool {
+//		let mut negative = false;
+//		for i in &self.coords {
+//			negative = i.is_negative() || negative
+//		}
+//		negative
+//	}
+//}
