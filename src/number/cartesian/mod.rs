4465411b2ae3328d2f27bd6760eae61c1c26f92c
bossmanTM <chase2jackman@gmail.com>
2024-07-24 21:01:18 -0600

    implimented generic sub for cartesian coordinates

--- src/number/cartesian/mod.rs
+++ src/number/cartesian/mod.rs
@@ -23,23 +23,15 @@ impl<T: Add<Output = T> + Copy> Add for Cartesian<T> {
 	}
 }
 
-//implimenting subtraction for my cartesian coordinate system
-macro_rules! impl_sub {
-	($($t:ty)*) => ($(
-		impl Sub for Cartesian< $t > {
-			type Output = Cartesian<$t>;
-			
-			fn sub(self, rhs: Self) -> Self::Output {
-				let mut sum:Cartesian<$t> = Cartesian::new();
-				for i in 0..self.coords.len() {
-					sum.coords.push(&self.coords[i] - &rhs.coords[i])
-				}
-				sum
-			}
-		}
-		
-	)*)
-}
+//implimenting generic sub for Cartesian
+impl<T: Sub<Output = T> + Copy> Sub for Cartesian<T> {
+	type Output = Self;
 
-// apparently i need to impliment it one at a time, ig rust aint got it all
-impl_sub! { usize u8 u16 u32 u64 u128 isize i8 i16 i32 i64 i128 f32 f64 }
+	fn sub(self, rhs: Self) -> Self::Output {
+		let mut sum:Cartesian<T> = Cartesian::new();
+		for i in 0..self.coords.len() {
+			sum.coords.push(self.coords[i] - rhs.coords[i])
+		}
+		sum
+	}
+}
