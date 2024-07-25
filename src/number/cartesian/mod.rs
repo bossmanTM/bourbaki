2bc7a5295ce418ab6730f01e4a1a197715745eb1
bossmanTM <chase2jackman@gmail.com>
2024-07-24 20:41:04 -0600

    implimented add for cartesian coordinates

--- src/number/cartesian/mod.rs
+++ src/number/cartesian/mod.rs
@@ -1,3 +1,4 @@
+use std::ops::Add; 
 
 pub struct Cartesian<T> {
 	pub coords :Vec<T>
@@ -8,3 +9,25 @@ impl<T> Cartesian<T> {
 		Cartesian {coords : Vec::new()}
 	}
 }
+
+
+//implimenting add for my cartesian coordinate system
+macro_rules! impl_add {
+	($($t:ty)*) => ($(
+		impl Add for Cartesian< $t > {
+			type Output = Cartesian<$t>;
+			
+			fn add(self, rhs: Self) -> Self::Output {
+				let mut sum:Cartesian<$t> = Cartesian::new();
+				for i in 0..self.coords.len() {
+					sum.coords.push(&self.coords[i] + &rhs.coords[i])
+				}
+				sum
+			}
+		}
+		
+	)*)
+}
+
+// apparently i need to impliment it one at a time, ig rust aint got it all
+impl_add! { usize u8 u16 u32 u64 u128 isize i8 i16 i32 i64 i128 f32 f64 }
